7a9c7e699a2905785569aba7e1328beaf629c37c
meskise <meskise@limestone.on.ca>
2022-05-31 07:54:02 -0400

    Added MyWorld and Images

---
+++ Cycle 3 Summative - Protective Orb/MyWorld.java
@@ -0,0 +1,35 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class MyWorld here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class MyWorld extends World
+{
+    
+    /**
+     * Constructor for objects of class MyWorld.
+     * 
+     */
+    public MyWorld()
+    {    
+        super(600, 400, 1); 
+        spawnObjects();
+    }
+    
+    public void act()
+    {
+        
+    }
+    
+    public void spawnObjects()
+    {
+        Woodcutter woodcutter = new Woodcutter();
+        addObject (woodcutter, 250, 250);
+    }
+    
+    
+    
+}
