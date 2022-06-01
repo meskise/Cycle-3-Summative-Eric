8238479be42c1a30237311b2f02e6530e67e0295
meskise <meskise@limestone.on.ca>
2022-06-01 10:58:07 -0400

    Added animations for enemy
    havent fixed attacking fully

--- Cycle 3 Summative - Protective Orb/Enemy.java
+++ Cycle 3 Summative - Protective Orb/Enemy.java
@@ -8,29 +8,115 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Enemy extends Actor
 {
+     SimpleTimer shotTimer = new SimpleTimer();
+    
+    // Sword Throwing
+    int randomShot = Greenfoot.getRandomNumber(6000);
+    int minShotWait = 1000;
+    
     int enemyFrameCounter = 0;
+    int counter = 0;
+    
     double deltaX = -2;
     double deltaY = 0;
     int health = 200;
     
     int animationInterval = 5;
     
+    GreenfootImage imageWalkLeft1;
+    GreenfootImage imageWalkLeft2;
+    GreenfootImage imageWalkLeft3;
+    GreenfootImage imageWalkLeft4;
+    GreenfootImage imageWalkLeft5;
+    GreenfootImage imageWalkLeft6;
+    GreenfootImage imageWalkLeft7;
+    GreenfootImage imageWalkLeft8;
+      
+    GreenfootImage imageWalkRight1;
+    GreenfootImage imageWalkRight2;
+    GreenfootImage imageWalkRight3;
+    GreenfootImage imageWalkRight4;
+    GreenfootImage imageWalkRight5;
+    GreenfootImage imageWalkRight6;
+    GreenfootImage imageWalkRight7;
+    GreenfootImage imageWalkRight8;
+    
     GreenfootImage imageDying1;
     GreenfootImage imageDying2;
     GreenfootImage imageDying3;
+    
+    GreenfootImage imageAttack1;
+    GreenfootImage imageAttack2;
+    GreenfootImage imageAttack3;
+    GreenfootImage imageAttack4;
+    
     public Enemy()
     {
-        GreenfootImage image = getImage();
-        image.scale(50, 50);
-        setImage(image);
+        // Load images.
+        imageWalkLeft1 = new GreenfootImage("EnemyWalking01.png");
+        imageWalkLeft2 = new GreenfootImage("EnemyWalking02.png");
+        imageWalkLeft3 = new GreenfootImage("EnemyWalking03.png");
+        imageWalkLeft4 = new GreenfootImage("EnemyWalking04.png");
+        imageWalkLeft5 = new GreenfootImage("EnemyWalking05.png");
+        imageWalkLeft6 = new GreenfootImage("EnemyWalking06.png");
+        imageWalkLeft7 = new GreenfootImage("EnemyWalking07.png");
+        imageWalkLeft8 = new GreenfootImage("EnemyWalking08.png");
+        // Images are the same for walking right, just mirrored.
+        imageWalkRight1 = new GreenfootImage("EnemyWalking01.png");
+        imageWalkRight2 = new GreenfootImage("EnemyWalking02.png");
+        imageWalkRight3 = new GreenfootImage("EnemyWalking03.png");
+        imageWalkRight4 = new GreenfootImage("EnemyWalking04.png");
+        imageWalkRight5 = new GreenfootImage("EnemyWalking05.png");
+        imageWalkRight6 = new GreenfootImage("EnemyWalking06.png");
+        imageWalkRight7 = new GreenfootImage("EnemyWalking07.png");
+        imageWalkRight8 = new GreenfootImage("EnemyWalking08.png");
         
         imageDying1 = new GreenfootImage("EnemyDying01.png");
         imageDying2 = new GreenfootImage("EnemyDying02.png");
         imageDying3 = new GreenfootImage("EnemyDying03.png");
         
+        imageAttack1 = new GreenfootImage("EnemyAttack01.png");
+        imageAttack2 = new GreenfootImage("EnemyAttack02.png");
+        imageAttack3 = new GreenfootImage("EnemyAttack03.png");
+        imageAttack4 = new GreenfootImage("EnemyAttack04.png");
+        
         imageDying1.scale(50, 32);
         imageDying2.scale(50, 32);
         imageDying3.scale(50, 32);
+        
+        imageAttack1.scale(45, 50);
+        imageAttack2.scale(45,50);
+        imageAttack3.scale(50,50);
+        imageAttack4.scale(50,50);
+        
+        // Mirrors images for walking right.
+                imageWalkRight1.mirrorHorizontally();
+                imageWalkRight2.mirrorHorizontally();
+                imageWalkRight3.mirrorHorizontally();
+                imageWalkRight4.mirrorHorizontally();
+                imageWalkRight5.mirrorHorizontally();
+                imageWalkRight6.mirrorHorizontally();
+                imageWalkRight7.mirrorHorizontally();
+                imageWalkRight8.mirrorHorizontally();
+                
+                // Changes the size of the images.
+                imageWalkLeft1.scale(45, 45);
+                imageWalkLeft2.scale(45, 45);
+                imageWalkLeft3.scale(45, 45);
+                imageWalkLeft4.scale(45, 45);
+                imageWalkLeft5.scale(45, 45);
+                imageWalkLeft6.scale(45, 45);
+                imageWalkLeft7.scale(45, 45);
+                imageWalkLeft8.scale(45, 45);
+                
+                imageWalkRight1.scale(45, 45);
+                imageWalkRight2.scale(45, 45);
+                imageWalkRight3.scale(45, 45);
+                imageWalkRight4.scale(45, 45);
+                imageWalkRight5.scale(45, 45);
+                imageWalkRight6.scale(45, 45);
+                imageWalkRight7.scale(45, 45);
+                imageWalkRight8.scale(45, 45);
     }
     
     /**
@@ -41,6 +127,27 @@ public class Enemy extends Actor
     {
         isTouching();
         health();
+        swordThrow();
+        walkLeft();
+        walkRight();
+        
+        enemyFrameCounter++;
+        if (enemyFrameCounter >= 8 * animationInterval)
+         {
+             // Reset to 0.
+             enemyFrameCounter = 0;
+         }
+    }
+    
+    public void swordThrow()
+    {
+        //System.out.println(counter);
+        if (shotTimer.millisElapsed() > minShotWait + randomShot)
+        {
+            shotTimer.mark();
+            randomShot = Greenfoot.getRandomNumber(3000);
+            enemyAttack();
+        }
     }
     
     public void health()
@@ -76,7 +183,7 @@ public class Enemy extends Actor
          if (enemyFrameCounter >= 8 * animationInterval)
          {
              // Reset to 0.
-             enemyFrameCounter = 0;
+             //enemyFrameCounter = 0;
              getWorld().removeObject(this);
          }
                 
@@ -114,7 +221,150 @@ public class Enemy extends Actor
          }
                 
          // Increment frame counter.
-         enemyFrameCounter++;
+         //enemyFrameCounter++;
     }
     
+    public void enemyAttack()
+    {
+        deltaX = 0.5;
+        // System.out.println(enemyFrameCounter);
+        if (enemyFrameCounter >= 8 * animationInterval)
+         {
+             // Reset to 0.
+             //enemyFrameCounter = 0;
+         }
+                
+         if (enemyFrameCounter == 0)
+         {
+             setImage(imageAttack1);
+         }
+         else if (enemyFrameCounter == 1 * animationInterval)
+         {
+             setImage(imageAttack1);
+         }
+         else if (enemyFrameCounter == 2 * animationInterval)
+         {
+             setImage(imageAttack2);
+         }
+         else if (enemyFrameCounter == 3 * animationInterval)
+         {
+             setImage(imageAttack2);
+         }
+         else if (enemyFrameCounter == 4 * animationInterval)
+         {
+             setImage(imageAttack3);
+         }
+         else if (enemyFrameCounter == 5 * animationInterval)
+         {
+             setImage(imageAttack3);
+         }
+         else if (enemyFrameCounter == 6 * animationInterval)
+         {
+             setImage(imageAttack3);
+         }
+         else if (enemyFrameCounter == 7 * animationInterval)
+         {
+             setImage(imageAttack4);
+             getWorld().addObject(new Sword(), getX(), getY());
+         }
+   
+         // Increment frame counter.
+         //enemyFrameCounter++;
+    }
+    
+    public void walkLeft()
+            {
+                if (deltaX < 0)
+                {
+                    if (enemyFrameCounter >= 8 * animationInterval)
+                    {
+                        // Reset to 0.
+                        //enemyFrameCounter = 0;
+                    }
+                
+                    if (enemyFrameCounter == 0)
+                    {
+                        setImage(imageWalkLeft1);
+                    }
+                    else if (enemyFrameCounter == 1 * animationInterval)
+                    {
+                        setImage(imageWalkLeft2);
+                    }
+                    else if (enemyFrameCounter == 2 * animationInterval)
+                    {
+                        setImage(imageWalkLeft3);
+                    }
+                    else if (enemyFrameCounter == 3 * animationInterval)
+                    {
+                        setImage(imageWalkLeft4);
+                    }
+                    else if (enemyFrameCounter == 4 * animationInterval)
+                    {
+                        setImage(imageWalkLeft5);
+                    }
+                    else if (enemyFrameCounter == 5 * animationInterval)
+                    {
+                        setImage(imageWalkLeft6);
+                    }
+                    else if (enemyFrameCounter == 6 * animationInterval)
+                    {
+                        setImage(imageWalkLeft7);
+                    }
+                    else if (enemyFrameCounter == 7 * animationInterval)
+                    {
+                        setImage(imageWalkLeft8);
+                    }
+                
+                    // Increment frame counter.
+                    //enemyFrameCounter++;
+                }
+            }
+            
+     public void walkRight()
+            {
+                if (deltaX > 1)
+                {
+                    if (enemyFrameCounter >= 8 * animationInterval)
+                    {
+                    // Reset to 0.
+                    //enemyFrameCounter = 0;
+                    }
+            
+                    if (enemyFrameCounter == 0)
+                    {
+                        setImage(imageWalkRight1);
+                    }
+                    else if (enemyFrameCounter == 1 * animationInterval)
+                    {
+                        setImage(imageWalkRight2);
+                    }
+                    else if (enemyFrameCounter == 2 * animationInterval)
+                    {
+                        setImage(imageWalkRight3);
+                    }
+                    else if (enemyFrameCounter == 3 * animationInterval)
+                    {
+                        setImage(imageWalkRight4);
+                    }
+                    else if (enemyFrameCounter == 4 * animationInterval)
+                    {
+                        setImage(imageWalkRight5);
+                    }
+                    else if (enemyFrameCounter == 5 * animationInterval)
+                    {
+                        setImage(imageWalkRight6);
+                    }
+                    else if (enemyFrameCounter == 6 * animationInterval)
+                    {
+                        setImage(imageWalkRight7);
+                    }
+                    else if (enemyFrameCounter == 7 * animationInterval)
+                    {
+                        setImage(imageWalkRight8);
+                    }
+                } 
+        
+                // Increment frame counter.
+                //enemyFrameCounter++;
+            }
 }
---
+++ Cycle 3 Summative - Protective Orb/Sword.java
@@ -0,0 +1,54 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Sword here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Sword extends Actor
+{
+    public Sword()
+    {
+        // Gets the sword image to resize it.
+        GreenfootImage image = getImage();
+        image.scale(30, 13);
+        setImage(image);
+    }
+    
+    /**
+     * Act - do whatever the Sword wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {   
+        // Move.
+        move(-4);
+
+        // If touches woodcutter remove woodcutter and sword, set world to game over.
+        Woodcutter w = (Woodcutter) getOneIntersectingObject(Woodcutter.class);
+        if (w != null)
+        {
+            getWorld().removeObject(w);
+            getWorld().removeObject(this);
+            Greenfoot.setWorld(new GameOver());
+        }
+        else if (getY() > 390)
+        {
+            getWorld().removeObject(this);
+        }
+        else if (getY() < 1)
+        {
+            getWorld().removeObject(this);
+        }
+        else if (getX() > 595)
+        {
+            getWorld().removeObject(this);
+        }
+        else if (getX() < 1)
+        {
+            getWorld().removeObject(this);
+        }
+    
+    }
+}
